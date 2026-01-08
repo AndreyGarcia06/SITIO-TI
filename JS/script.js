8511a76a3c9169716baa116a7b17fd3577a04e5a
Andrey Ortega Garcia <240370@utxicotepec.edu.mx>
2026-01-08 14:29:33 -0600

    update: Se agregaron funciones js y estilo del encabezado con css

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,31 @@
 function saludar (){
     alert("Bienvenidos al himalaya");
 }
+
+function suma(){
+   var a = 5;
+   var b = 10;
+   var c = a + b;
+   alert ("El resultado de la suma "+ a + " + " + b + " es = " + c);
+}
+
+function resta(){
+   var a = 5;
+   var b = 10;
+   var c = b - a;
+   alert ("El resultado de la resta "+ b + " - " + a + " es = " + c);
+}
+
+function multiplicacion(){
+   var a = 5;
+   var b = 10;
+   var c = a * b;
+   alert ("El resultado de la multiplicacion "+ a + " * " + b + " es = " + c);
+}
+
+function division(){
+   var a = 5;
+   var b = 10;
+   var c = b / a;
+   alert ("El resultado de la division "+ b + " / " + a + " es = " + c);
+}
